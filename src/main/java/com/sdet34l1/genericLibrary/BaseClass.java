b6968a220bb693a175c28251df56b63c113955eb
Niraj <Niraj@DESKTOP-RB30UFV>
2022-05-30 22:54:56 +0530

    niraj updated

--- src/main/java/com/sdet34l1/genericLibrary/BaseClass.java
+++ src/main/java/com/sdet34l1/genericLibrary/BaseClass.java
@@ -24,11 +24,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
  * This class is used to common activities of the scripts in the form of annotations
  * @author Niraj
  * This is the supermost class of all scripts
-
- * Change done in niraj
-
- * Just try to make conflict
-
  */
 public class BaseClass {
 	public WebDriverDataLibrary webDriverDataLibrary;
